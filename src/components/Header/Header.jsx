9eee365e5e3e62c58217f062fc9f8a78e557a7ab
Sozoeva <sozoeva.alina@gmail.com>
2023-10-26 14:07:11 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
-import styles from "./Header.module.scss"
+import React, { useState } from "react";
+import styles from "./Header.module.scss";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import name from "../../assets/images/name.png";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className={`${styles.header__content} ${"container"}`}>
+        <nav className={styles.header__nav}>
+          <div className={styles.header__nav_logo}>
+            <div className={styles.logo}>
+            <Link to="/">
+              <img src={logo} alt="logo"  />
+            </Link>
+            </div>
+
+            <div className={styles.name}>
+              <img src={name} alt="name" />
+            </div>
+          </div>
+          <div className={styles.header__div}>
+            <div className={styles.header__nav_links}>
+              <Link className={styles.header___nav_link} to="/about">
+                About
+              </Link>
+              <Link className={styles.header__link} to="/history">
+                Hispory
+              </Link>
+              <Link className={styles.header__link} to="/characters">
+                Characters
+              </Link>
+              <Link className={styles.header__link} to="/forum">
+                Forum
+              </Link>
+              <Link className={styles.header__link} to="/questions">
+                FAQ
+              </Link>
+            </div>
+            <div className={styles.header__nav_btns}>
+              <div>
+                <button>EN</button>
+              </div>
+              <Link to="/register">
+                <button>Register/Login</button>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
