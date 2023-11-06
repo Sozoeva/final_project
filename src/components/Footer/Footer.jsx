ef64eb9fb34c458e82bf969030d10a798d746f30
Sozoeva <sozoeva.alina@gmail.com>
2023-11-06 18:06:36 +0600

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
-import styles from "./Footer.module.scss"
+import React from "react";
+import styles from "./Footer.module.scss";
+import {
+  AiFillYoutube,
+  AiOutlineInstagram,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import { BsTiktok, BsDiscord } from "react-icons/bs";
+import { SiTwitch } from "react-icons/si";
 
 export const Footer = () => {
   return (
-    <div className={styles.footer}>  Footer</div>
-  )
-}
+    <section className={styles.footer}>
+      <div className={`${styles.footer__content} ${"container"}`}>
+        <div className={styles.footer__content_media}>
+          <div>
+            <AiFillYoutube />
+          </div>
+          <div>
+            <AiOutlineInstagram />
+          </div>
+          <div>
+            <AiFillTwitterCircle />
+          </div>
+          <div>
+            <BsTiktok />
+          </div>
+          <div>
+            <SiTwitch />
+          </div>
+          <div>
+            <BsDiscord />
+          </div>
+        </div>
+        <div className={styles.footer__content_title}>
+          <div className={styles.footer__content_title_about}>
+            <p>
+              MORTAL KOMBAT 1 Software © 2023 Warner Bros. Entertainment Inc.
+              Developed by NetherRealm Studios. Unreal® Engine, copyright
+              1998-2023 Epic Games, Inc. Unreal, Unreal Technology and the
+              Powered by Unreal Technology logo are trademarks or registered
+              trademarks of Epic Games, Inc. Uses Oodle Data Compression
+              Copyright and Bink Video (C) 1997-2023 by RAD Game Tools, Inc.
+              NETHERREALM STUDIOS LOGO, MORTAL KOMBAT, THE DRAGON LOGO, and all
+              related characters and elements are trademarks of and © 2023
+              Warner Bros. Entertainment Inc.
+            </p>
+            <div>
+              <img
+                src="https://cdn-mk1.mortalkombat.com/static/wb-shield-white.svg"
+                alt=""
+                className={styles.footer__content_title_about_WB}
+              />
+              <span>
+                WARNER BROS. GAMES LOGO, WARNER BROS. INTERACTIVE LOGO, WB
+                SHIELD: ™ & © Warner Bros. Entertainment Inc. (s23)
+              </span>
+            </div>
+          </div>
+          <img
+            src="https://cdn-mk1.mortalkombat.com/ratings/esrb/esrb.svg"
+            alt=""
+            className={styles.footer__content_title_ERSB}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
